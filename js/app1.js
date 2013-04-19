bf958f2c461caeee2d0aef8620cc80e4f91f43a6
xiaoming qin <qinxm2000@yahoo.com>
2013-04-19 12:58:46 -0300

    Update app1.js

--- js/app1.js
+++ js/app1.js
@@ -621,3 +621,15 @@ d3.json("data/trafficBilledInUS.json", function(d){
 	drawYears(yearArray);
 	drawTimeline(true);
 });
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-40170701-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
